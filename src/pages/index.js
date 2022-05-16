4a9451afa27589f42aad360324193ef98b09d35e
freddy castuera <freddycastuera@MacBook-Air-de-freddy.local>
2022-05-16 11:04:32 -0600

    agregamos un plugin para las imagenes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import MyLayout from "../components/MyLayout"
+import { StaticImage } from "gatsby-plugin-image"
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -9,6 +10,10 @@ const IndexPage = () => {
     <MyLayout pageTitle="home">
       <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="imagen bien perrona"
+        src="http://placekitten.com/g/500/500"
+      />
     </MyLayout>
   )
 }
